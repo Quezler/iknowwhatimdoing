98985fd145767e41e3239206d3bbf32eb1f71aad
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-25 20:37:32 +0200

    Thank anuke

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,7 @@ Events.on(EventType.ClientLoadEvent, run(() => {
     Core.app.post(run(() => {
         Core.app.post(run(() => {
             Core.app.post(run(() => {
-                children = Core.scene.root.getChildren();
-                children.reverse();
-                children.first().hide();
-                children.reverse();
+                Core.scene.getDialog().hide();
             }));
         }));
     }));
